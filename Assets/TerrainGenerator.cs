d651a3667f5acd11b5687d5a63f9824dd51419e6
Chytrvs <marcinchytrzynski@gmail.com>
2017-06-03 20:33:28 +0200

    Bugfix, added option to use rounded height

--- Assets/TerrainGenerator.cs
+++ Assets/TerrainGenerator.cs
@@ -1,143 +1,129 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//[ExecuteInEditMode]
-public class TerrainGenerator : MonoBehaviour {
+
+public class TerrainGenerator : MonoBehaviour
+{
     public int width;
     public float heightScale;
     public float intensity;
-    public float Offset;
+    public float xOffset, zOffset;
     public float triangleScale;
+    int[] triangles;
+    Vector3[] Norm;
+    public bool RoundHeight;
 
     void Start()
     {
         gameObject.AddComponent<MeshFilter>();
         Mesh mesh = GetComponent<MeshFilter>().mesh;
-        mesh.vertices = GenerateVertices();
-        mesh.triangles = GenerateTriangles();
-        mesh.normals = GenerateNormals();
+        mesh.vertices = GenerateVerticies();
+        mesh.normals = Norm;
+        mesh.triangles = triangles;
     }
-    void Update () {
+    void Update()
+    {
         Mesh mesh = GetComponent<MeshFilter>().mesh;
-        mesh.vertices = GenerateVertices();
-        mesh.triangles = GenerateTriangles();
-        mesh.normals = GenerateNormals();
-
-    }
-    Vector3[] GenerateVertices()
-    {
-        Vector3[] Grid = new Vector3[width*width];
-        int z=1;
-        int x=1;
-        for (int i = 0; i < Grid.Length; i++)
-        {
-            if (x < width)
-            {
-            Grid[i] = new Vector3(x*triangleScale, GenerateHeight(x,z), z* triangleScale);
-            x++;
-
-            }
-            else
-            {
-                Grid[i] = new Vector3(x* triangleScale, GenerateHeight(x, z), z* triangleScale);
-                x = 1;
-                z++;
-            }
-
-        }
-        return Grid;
-    }
-    Vector3[] GenerateNormals()
-    {
-      
-        Vector3[] norm = new Vector3[width * width];
-        int z = 1;
-        int x = 1;
-        for (int i = 0; i < norm.Length; i++)
-        {
-            if (x < width)
-            {
-                norm[i] = Vector3.up;
-                x++;
-
-            }
-            else
-            {
-               
-                norm[i] = Vector3.up;
-                x = 1;
-                z++;
-            }
-
-        }
-        return norm;
-    }
-    int[] GenerateTriangles()
-    {
-        int[] triangles = new int[((((width-1) * (width - 1)) * 3) )* 2];
-        int b = 0;
-        int c = 0;
-        int z = 0;
-        for (int i = 0; i < (triangles.Length/2); i++)
-        {
-            if ((z + 1) % width == 0)
-            {
-                z++;
-            }
-                if (b == 0)
-                {
-                    triangles[i] = z;
-                    b++;
-                }
-                else if (b == 1)
-                {
-
-                    triangles[i] = z + width;
-                    b++;
-                }
-                else if (b == 2)
-                {
-                    triangles[i] = z + 1;
-                    b = 0;
-                    z++;
-                }
-
-        }
-         b = 0;
-         z = 1;
-        for (int i = triangles.Length / 2; i < triangles.Length; i++)
-        {
-            if (z % width == 0)
-            {
-                z++;
-            }
-            if (b == 0)
-            {
-                triangles[i] = (z + width) - 1;
-
-
-                b++;
-            }
-            else if (b == 1)
-            {
-
-                triangles[i] = z + width;
-                b++;
-            }
-            else if (b == 2)
-            {
-                triangles[i] = z;
-                b = 0;
-                z++;
-            }
-        }
-        return triangles;
+        mesh.vertices = GenerateVerticies();
+        mesh.normals = Norm;
+        mesh.triangles = triangles;
     }
     float GenerateHeight(int x, int z)
     {
-        float xCoord = (float)x / width*intensity+Offset;
-        float zCoord = (float)z / width*intensity+Offset*2;
-        float y = Mathf.PerlinNoise(xCoord, zCoord)*heightScale;
-        return y;
+        float xCoord = (float)x / width * intensity + xOffset;
+        float zCoord = (float)z / width * intensity + zOffset;
+        float y = Mathf.PerlinNoise(xCoord, zCoord) * heightScale;
+        if (RoundHeight)
+            return Mathf.RoundToInt(y);
+        else
+            return y;
+    }
+    Vector3[] GenerateVerticies()
+    {
+        triangles = new int[width * width * 6]; //each square has 6 verticies and triangle points
+        Vector3[] Grid = new Vector3[triangles.Length];
+        Norm = new Vector3[triangles.Length];
+        int TriangleIteration = 0;
+        int x = 0;
+        int y = 0;
+        for (int VertexIndex = 0; VertexIndex < triangles.Length / 2; VertexIndex++) //For each iteration sets another point of triangle
+        {
+            if (TriangleIteration == 0)
+            {
+                Grid[VertexIndex] = new Vector3(x, GenerateHeight(x, y), y);
+                triangles[VertexIndex] = VertexIndex;
+                Norm[VertexIndex] = Vector3.up;
+
+            }
+            if (TriangleIteration == 1)
+            {
+                Grid[VertexIndex] = new Vector3(x, GenerateHeight(x, y + 1), y + 1);
+                triangles[VertexIndex] = VertexIndex;
+                Norm[VertexIndex] = Vector3.up;
+
+            }
+            if (TriangleIteration == 2)
+            {
+                Grid[VertexIndex] = new Vector3(x + 1, GenerateHeight(x + 1, y), y);
+                triangles[VertexIndex] = VertexIndex;
+                Norm[VertexIndex] = Vector3.up;
+                TriangleIteration = 0;
+                if (x != width - 1)
+                {
+                    x++;
+                }
+                else
+                {
+                    x = 0;
+                    y++;
+                }
+            }
+            else
+            {
+                TriangleIteration++;
+            }
+
+        }
+        x = 1;
+        y = 0;
+        TriangleIteration = 0;
+        for (int VertexIndex = triangles.Length / 2; VertexIndex < triangles.Length; VertexIndex++)
+        {
+            if (TriangleIteration == 0)
+            {
+                Grid[VertexIndex] = new Vector3(x, GenerateHeight(x, y), y);
+                triangles[VertexIndex] = VertexIndex;
+                Norm[VertexIndex] = Vector3.up;
+
+            }
+            if (TriangleIteration == 1)
+            {
+                Grid[VertexIndex] = new Vector3(x - 1, GenerateHeight(x - 1, y + 1), y + 1);
+                triangles[VertexIndex] = VertexIndex;
+                Norm[VertexIndex] = Vector3.up;
+
+            }
+            if (TriangleIteration == 2)
+            {
+                Grid[VertexIndex] = new Vector3(x, GenerateHeight(x, y + 1), y + 1);
+                triangles[VertexIndex] = VertexIndex;
+                Norm[VertexIndex] = Vector3.up;
+                TriangleIteration = 0;
+                if (x != width)
+                {
+                    x++;
+                }
+                else
+                {
+                    x = 1;
+                    y++;
+                }
+            }
+            else
+            {
+                TriangleIteration++;
+            }
+        }
+        return Grid;
     }
 }
